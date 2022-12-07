2590d026591a9556d2a3c90b09854cf4b42545c7
Chloris <chloris@freedommail.ch>
2022-12-07 13:18:26 +0100

    Add AT scope training materials

--- training/scopes.php
+++ training/scopes.php
@@ -9,8 +9,41 @@ $meta = new PageMeta(
 
 begin_page($meta);
 ?>
+<section>
+    <h2>Ranging</h2>
+
+    <div class='icons-horizontal wide big-icons'>
+        <div>
+            <h5>Vehicle Width</h5>
+            <img src='/img/icons/range_us.svg'>
+            <p>Variable width boxes in the center.</p>
+            <p>Width at set distance.</p>
+            <p>Aim with the range marker.</p>
+            <p>Typically on NATO scopes.</p>
+        </div>
+        <div>
+            <h5>Vehicle Height</h5>
+            <img src='/img/icons/range_ru.svg'>
+            <p>Graph markings to one side.</p>
+            <p>Height at a set distance.</p>
+            <p>Ajust zeroing or use grid to aim.</p>
+            <p>Typically on Russian scopes.</p>
+        </div>
+    </div>
+</section>
+
 <section class='narrow'>
-Placeholder.
+    <h2>Aiming</h2>
+
+    <figure class='limited-height'>
+        <img src='/img/figures/RPG7_Scope.svg'>
+        <figcaption>RPG-7 scope.</figcaption>
+    </figure>
+
+    <figure class='limited-height size-400'>
+        <img src='/img/figures/RGW90.svg'>
+        <figcaption>RGW 90 scope.</figcaption>
+    </figure>
 </section>
 <?php
 end_page($meta);
